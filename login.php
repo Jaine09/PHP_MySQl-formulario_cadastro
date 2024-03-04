fc0200a29e239ee60f4ceacdfb4fb10b9edc6270
Jaine09 <jainejesus0911@gmail.com>
2024-03-04 15:00:33 -0300

    .

--- login.php
+++ login.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de Login</title>
-    <link rel="stylesheet" href="telalogin.css">
+    <link rel="stylesheet" href="telalogin.css">    
 </head>
 <body>
     <div class="box">
-        <h1>Login</h1>
+        <h1>Realize o Login</h1>
         <br><br>
         <form action="testarlogar.php" method="POST">
             <input type="text" name="email" placeholder="E-mail" class="inputUser">
